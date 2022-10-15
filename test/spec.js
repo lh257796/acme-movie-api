311ecc8e60d83ea54aa1c3c71a06ccc3759c059a
Han <hl2341@nyu.edu>
2022-10-15 11:06:35 -0400

    .yml added?

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,7 @@
 const { assert } = require("chai")
 
 describe('Testing Hello', () => {
-    describe('1 test passed!!!', () => {
+    describe('First ever test here:', () => {
         it('should return a green checkmark if this test is present', () => {
             assert.equal(true,1)
         })
