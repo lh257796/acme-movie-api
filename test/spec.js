26f25f647ab6bd3f8d2474adf9d116554ccfd4f2
Han <hl2341@nyu.edu>
2022-10-15 10:52:18 -0400

    added test

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+const { assert } = require("chai")
+
+describe('Testing Hello', () => {
+    describe('1 test passed!!!', () => {
+        it('should return a green checkmark if this test is present', () => {
+            assert.equal(true,1)
+        })
+    })
+})
